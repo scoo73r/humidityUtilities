57d0d086e6cfdeed1bb432c0ca4d7f6a8fe9b8ae
Scooter <scooter@hey.com>
2021-01-01 18:18:29 -0600

    finish first implementation

--- include/humidity.h
+++ include/humidity.h
@@ -9,62 +9,67 @@ private:
     const double _bw = 18.678;
     const double _cw = 257.14;
     const double _dw = 234.5;
+
     //Constants over Ice
     const double _ai = 6.1115;
     const double _bi = 23.036;
     const double _ci = 279.82;
     const double _di = 333.7;
 
-    //Pressure level in millibars
-    //Currently defaulted to just above sea level.
+    //enhanmentFactors over water and ice
+    const double _efaw = 2.2;
+    const double _efbw = 0.0383;
+    const double _efcw = 6.4;
+
+    const double _efai = 7.2;
+    const double _efbi = 0.0320;
+    const double _efci = 5.9;
+
+    //air pressure in millibars
     double _airPressure = 1010.0;
 
-    double f1AboveFreezing(double dp);
-    double f1BelowFreezing(double dp);
-    double f2AboveFreezing(double e);
-    double f1BelowFreezing(double e);
+    double f1(double temp);
+    double f2(double vaporPressure, bool aboveFreezing);
+    double enhancmentFactor(double temp);
+    double enhancmentFactor(double temp, double a, double b, double c);
+
+    double f1(double temp, double a, double b, double c, double d);
+    double f2(double vaporPressure, double a, double b, double c, double d);
+
+    double convertCelsiusToKelvin(double tempatureCelsius);
+    double convertKelvinToCelsius(double tempatureKelvin);
 
 public:
     Humidity();
     ~Humidity();
-    /*
-    DP = dew or frost point in deg C
-    e = vapor pressure in millibars
-    es = saturation vapor pressure in millibars
-    P = pressure in millibars
-    r = mixing ratio by weight in ppm
-    RH = relative humidity in percent
-    rho = absolute humidity in g/m3
-    rhos = absolute humidity at saturation
-    T = temperature in deg C
-    Tk = absolute temperature in K
-*/
+
+    void setAirPressure(double airPressure);
     double getSaturationPressure(double tempatureCelsius);
 
-    double getDewPointFromVaporPressure(double vaporPressure);
-    double getDewPointFromMixingRatio(double mixingRatio);
-    double getDewPointFromRelativeHumidity(double relativeHumidity);
-    double getDewPointFromAbsoluteHumidity(double absoluteHumidity);
+    double getDewPointFromVaporPressure(double vaporPressure, bool aboveFreezing = true);
+    double getDewPointFromMixingRatio(double mixingRatio, bool aboveFreezing = true);
+    double getDewPointFromRelativeHumidity(double relativeHumidity, double tempatureCelsius);
+    double getDewPointFromAbsoluteHumidity(double absoluteHumidity, double tempatureKelvin);
 
-    double VaporPressureFromDewPoint(double dewPoint);
-    double VaporPressureFromMixingRatio(double mixingRatio);
-    double VaporPressureFromRelativeHumidity(double relativeHumidity);
-    double VaporPressureFromAbsoluteHumidity(double absoluteHumidity);
+    double getVaporPressureFromDewPoint(double dewPoint);
+    double getVaporPressureFromMixingRatio(double mixingRatio);
+    double getVaporPressureFromRelativeHumidity(double relativeHumidity, double tempatureCelsius);
+    double getVaporPressureFromAbsoluteHumidity(double absoluteHumidity, double tempatureKelvin);
 
-    double getMixingRatioFromVaporPressure(double vaporPressure);
-    double getMixingRatioFromDewPoint(double dewPoint);
-    double getMixingRatioFromRelativeHumidity(double relativeHumidity);
-    double getMixingRatioFromAbsoluteHumidity(double absoluteHumidity);
+    double getMixingRatioFromVaporPressure(double vaporPressure, double molecularWeight = 28.97);
+    double getMixingRatioFromDewPoint(double dewPoint, double molecularWeight = 28.97);
+    double getMixingRatioFromRelativeHumidity(double relativeHumidity, double tempatureCelsius, double molecularWeight = 28.97);
+    double getMixingRatioFromAbsoluteHumidity(double absoluteHumidity, double tempatureKelvin, double molecularWeight = 28.97);
 
-    double getRelativeHumidityFromVaporPressure(double vaporPressure);
-    double getRelativeHumidityFromMixingRatio(double mixingRatio);
-    double getRelativeHumidityFromDewPoint(double dewPoint);
-    double getRelativeHumidityFromAbsoluteHumidity(double absoluteHumidity);
+    double getRelativeHumidityFromVaporPressure(double vaporPressure, double tempatureCelsius);
+    double getRelativeHumidityFromMixingRatio(double mixingRatio, double tempatureCelsius);
+    double getRelativeHumidityFromDewPoint(double dewPoint, double tempatureCelsius);
+    double getRelativeHumidityFromAbsoluteHumidity(double absoluteHumidity, double tempatureKelvin);
 
-    double getAbsoluteHumidityFromVaporPressure(double vaporPressure);
-    double getAbsoluteHumidityFromMixingRatio(double mixingRatio);
-    double getAbsoluteHumidityFromRelativeHumidity(double relativeHumidity);
-    double getAbsoluteHumidityFromDewPoint(double dewPoint);
+    double getAbsoluteHumidityFromVaporPressure(double vaporPressure, double tempatureKelvin);
+    double getAbsoluteHumidityFromMixingRatio(double mixingRatio, double tempatureKelvin);
+    double getAbsoluteHumidityFromRelativeHumidity(double relativeHumidity, double tempatureCelsius);
+    double getAbsoluteHumidityFromDewPoint(double dewPoint, double tempatureKelvin);
 };
 
 Humidity::Humidity()
